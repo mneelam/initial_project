128d53555ecc68d6ad022ed2e3f300e4c9e7e327
Mahesh Babu <mahesh@Mahi-Mac.local>
2020-05-04 13:04:40 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "initial_project"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/mahesh.scala
@@ -0,0 +1,8 @@
+object mahesh {
+
+  def main(args: Array[String]): Unit = {
+    val nmb = 10
+    print(nmb)
+  }
+
+}
